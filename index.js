8b60df264bbc581fc15582857c6748513750c14a
carrie-riggs <carolynn.riggs@outlook.com>
2022-06-18 18:33:55 -0600

    Final Push

---
+++ index.js
@@ -0,0 +1,153 @@
+var board;
+var playerO = "O";
+var playerX = "X";
+var currPlayer = playerO;
+var gameOver = false;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [
+                [' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']
+            ]
+
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            if (r == 0 || r == 1) {
+                tile.classList.add("horizontal-line");
+            }
+            if (c == 0 || c == 1) {
+                tile.classList.add("vertical-line");
+            }
+            tile.innerText = "";
+            tile.addEventListener("click", setTile);
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
+
+var headingDiv = document.getElementById("turns");
+headingDiv.innerHTML = `<H3>Player ${currPlayer}'s Turn</H3>`;
+
+function setTile() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-");    //ex) "1-2" -> ["1", "2'"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (board[r][c] != ' ') { 
+        //already taken spot
+        return;
+    }
+    
+    board[r][c] = currPlayer; //mark the board
+    this.innerText = currPlayer; //mark the board on html
+
+    //change players
+    if (currPlayer == playerO) {
+        currPlayer = playerX;
+    }
+    else {
+        currPlayer = playerO;
+    }
+
+    var headingDiv = document.getElementById("turns");
+    headingDiv.innerHTML = `<H3>Player ${currPlayer}'s Turn</H3>`;
+
+    //check winner
+    checkWinner();
+}
+
+function declareWinner(currPlayer) {
+    if (currPlayer == playerO) {
+        currPlayer = playerX;
+    }
+    else {
+        currPlayer = playerO;
+    }
+    console.log('test 2');
+
+    //I don't know why this isn't showing an alert, but I ran out of time to troubleshoot it.
+    let alert = document.createElement('bootstrap-alert');
+    alert.className = 'alert alert-primary';
+    alert.innerHTML = `Player ${currPlayer} Wins!`;
+    return alert;
+}
+
+
+function checkWinner() {
+    //horizontally, check 3 rows
+    for (let r = 0; r < 3; r++) {
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
+            //if we found the winning row
+            //apply the winner style to that row
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(r.toString() + "-" + i.toString());
+                tile.classList.add("winner");
+            }
+            
+            declareWinner();
+            console.log('test 1');
+            
+            gameOver = true;
+            return;
+        }
+    }
+
+    //vertically, check 3 columns
+    for (let c = 0; c < 3; c++) {
+        if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
+            //if we found the winning col
+            //apply the winner style to that col
+            for (let i = 0; i < 3; i++) {
+                let tile = document.getElementById(i.toString() + "-" + c.toString());                
+                tile.classList.add("winner");
+            }
+
+            declareWinner();
+            gameOver = true;
+            return;
+        }
+    }
+
+    //diagonally
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+        for (let i = 0; i < 3; i++) {
+            let tile = document.getElementById(i.toString() + "-" + i.toString());                
+            tile.classList.add("winner");
+        }
+        gameOver = true;
+        return;
+    }
+
+    //anti-diagonally
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+
+        let tile = document.getElementById("0-2");                
+        tile.classList.add("winner");
+
+        tile = document.getElementById("1-1");                
+        tile.classList.add("winner");
+
+        tile = document.getElementById("2-0");                
+        tile.classList.add("winner");
+        gameOver = true;
+        return;
+    }
+}
+
+
+
+document.getElementById('reset').addEventListener('click', () => {
+    location.reload();
+})
